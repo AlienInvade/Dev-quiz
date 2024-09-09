470e7a1efaa7d1225ffc7ad8f79f986b89c020d9
Madona Wayne Ngobeni <madonawaynengobeni@gmail.com>
2024-09-09 21:13:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const navigationBar = document.querySelector("nav");
+
+navigationBar.addEventListener("mouseover", hover);
+navigationBar.addEventListener("mouseout", hoverOut);
+
+function hover() {
+  navigationBar.classList.add("black");
+  setTimeout( function () { 
+    navigationBar.classList.add("cursor")}, 500);
+}
+
+
+function hoverOut() {
+  navigationBar.classList.remove("black");
+  navigationBar.classList.remove("cursor");
+  
+}
+
