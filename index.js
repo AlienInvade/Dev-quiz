4926cfe720fa23a2d9d2d8892218e29862ecdb9b
Madona Wayne Ngobeni <madonawaynengobeni@gmail.com>
2024-09-21 14:46:46 +0200

    update

--- index.js
+++ index.js
@@ -20,4 +20,12 @@ function hoverOut() {
 
 // Menu function 
 
+function openMenu() {
+  document.getElementById('container').style.right='0%';
+  document.getElementById('menu').style.opacity='1';
+}
 
+function closeMenu() {
+  document.getElementById('container').style.right='-100%';
+  document.getElementById('menu').style.opacity='0';
+}
